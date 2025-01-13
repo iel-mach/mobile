b7c00510f83cfea6e6f632dbecb92092d5afe8e4
iel-mach <issam.elmachkour@outlook.com>
2025-01-13 04:51:29 +0100

    ex00 in day00

---
+++ day00/ex00/App.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function App() {
+
+  const handleClick = () => {
+    console.log("Button pressed")
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.te}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+      <Pressable style={styles.button} onPress={handleClick}>
+        <Text style={styles.textbutton}>Click me</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  te: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: wp(35),
+    height: hp(6),
+    marginVertical: 6,
+    borderRadius: wp(2),
+  },
+  text: {
+    color: '#fff',
+    fontSize: hp(3),
+    fontWeight: "800",
+  },
+  button: {
+    backgroundColor: "black",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: wp(4),
+    width: wp(24),
+    height: hp(5),
+  },
+  textbutton: {
+    color: 'orange',
+    fontSize: hp(2),
+  },
+});
